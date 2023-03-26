0abcd36d0f4a74c3a35c5926c1e6aad7a5877f6b
yumkx10 <yuji.i7211@gmail.com>
2023-03-26 12:00:33 +0900

    Bootstrapを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+import "bootstrap"
+import "../stylesheets/application"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
